f0722a8e58a6d18bd62cde22b0480c770a55f74d
max <maxsoncoelho@gmail.com>
2022-04-30 18:18:35 -0400

    install theme

--- src/screens/theme/index.ts
+++ src/screens/theme/index.ts
@@ -1,31 +1,18 @@
 export default {
-  COLORS: {
-    BACKGROUND: '#F7F7F7',
+  header: '#1B1B1F',
+  background_primary: '#F4F5F6',
+  background_secondary: '#FFFFFF',
 
-    PRIMARY_900: '#B83341',
-    PRIMARY_800: '#E03F50',
-    PRIMARY_100: '#D16470',
-    PRIMARY_50: '#FFABB3',
+  text: '#7A7A80',
+  text_detail: '#AEAEB3',
+  title: '#47474D',
 
-    SECONDARY_900: '#572D31',
-    SECONDARY_500: '#7A6769',
-    SECONDARY_400: '#93797B',
+  line: '#EBEBF0',
 
-    SUCCESS_900: '#528F33',
-    SUCCESS_50: '#F7FFF9',
+  main: '#DC1637',
+  main_light: '#FDEDEF',
+  success: '#03B252',
 
-    ALERT_900: '#B27F00',
-    ALERT_800: '#C5941A',
-    ALERT_50: '#F3EFE5',
-
-    SHAPE: '#DCDCDC',
-    TITLE: '#FFF',
-
-    GRADIENT: ['#B83341', '#E03F50'],
-  },
-
-  FONTS: {
-    TITLE: 'DMSerifDisplay_400Regular',
-    TEXT: 'DMSans_400Regular',
-  },
+  shape: '#E1E1E8',
+  shape_dark: '#29292E',
 };
